a5fe12a09291696167061a65a72b17cbcd189cdf
jea74 <jea74@drexel.edu>
2017-04-06 17:44:01 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+
   get 'users/new'
 
   get '/help', to: 'static_pages#help'
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  post '/signup',  to: 'users#create'
+
+  resources :users
 end
